42fdc5665a0d5e3d984d14618a768be88b0e1620
Lola-snt <lorrayne.work@gmail.com>
2024-05-21 17:41:38 -0300

    Iniciando Carrinho de Compras

--- react-web-ec-g1/src/App.tsx
+++ react-web-ec-g1/src/App.tsx
@@ -2,11 +2,8 @@ import { useState } from 'react'
 import './App.css'
 import { Routes } from 'react-router-dom'
 import { Route } from'react-router-dom'
-import Home from '../pages/Home' // falta export na Home (Kauan?)
-import Login from '../pages/Login' //falta export na pagina
-import Profile from '../pages/Profile' //falta export na pagina
-import Cart from '../pages/Cart' //falta export na pagina
-import Error from '../pages/Error' //falta export na pagina
+import { Cart } from './pages/Cart'
+
 
 
 function App() {
@@ -14,12 +11,12 @@ function App() {
 
   return (
     <Routes>
-    <Route path="/" element={<Home />} />
+    {/* <Route path="/" element={<Home />} />
     <Route path="/login" element={<Login />} />
-    <Route path="/profile" element={<Profile />} />
-    <Route path="cart"element={<Cart />} />
+    <Route path="/profile" element={<Profile />} /> */}
+    <Route path="cart" element={<Cart />} />
     {/* Pagina de 404 personalizada */}
-    <Route path="*" element={<Error />}  /> 
+    {/* <Route path="*" element={<Error />}  />  */}
   </Routes>
   )
 }
