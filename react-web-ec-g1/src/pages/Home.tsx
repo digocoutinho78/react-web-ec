10416282f8c374c4480167dbfa2d3fbe5059709f
digocoutinho78 <digocoutinho@outlook.com>
2024-05-23 16:09:26 -0300

    ajuste e teste home

--- react-web-ec-g1/src/pages/Home.tsx
+++ react-web-ec-g1/src/pages/Home.tsx
@@ -1,28 +1,60 @@
+import React from "react";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import ProductCard from "../components/CardProduto";
 
-
-import React from 'react';
-import { Container, Row, Col, Card, Button } from 'react-bootstrap';
-import ProductCard from '../components/CardProduto';
-
-
-export  function TesteHome () {
+export function TesteHome() {
   return (
-    <Container>
+    <Container className="conteiner">
       <Row>
-        
-        <Col>
-      <ProductCard imagem= 'https://m.media-amazon.com/images/I/41Q2NXnJALL.__AC_SX300_SY300_QL70_ML2_.jpg'  nome='Smartfone Multilaser' categoria='Celulares' descricao='Smartphone Multilaser E Pro 4G ' preco='R$ 1000,00' />
-        
-          </Col>
-        
+        <ProductCard
+          imagem="https://m.media-amazon.com/images/I/41Q2NXnJALL.__AC_SX300_SY300_QL70_ML2_.jpg"
+          nome="Smartfone Multilaser"
+          categoria="Celulares"
+          descricao="Smartphone Multilaser E Pro 4G "
+          preco="R$ 1000,00"
+        />
+        <ProductCard
+          imagem="https://m.media-amazon.com/images/I/41Q2NXnJALL.__AC_SX300_SY300_QL70_ML2_.jpg"
+          nome="Smartfone Multilaser"
+          categoria="Celulares"
+          descricao="Smartphone Multilaser E Pro 4G "
+          preco="R$ 1000,00"
+        />
+        <ProductCard
+          imagem="https://m.media-amazon.com/images/I/41Q2NXnJALL.__AC_SX300_SY300_QL70_ML2_.jpg"
+          nome="Smartfone Multilaser"
+          categoria="Celulares"
+          descricao="Smartphone Multilaser E Pro 4G "
+          preco="R$ 1000,00"
+        />
+        <ProductCard
+          imagem="https://m.media-amazon.com/images/I/41Q2NXnJALL.__AC_SX300_SY300_QL70_ML2_.jpg"
+          nome="Smartfone Multilaser"
+          categoria="Celulares"
+          descricao="Smartphone Multilaser E Pro 4G "
+          preco="R$ 1000,00"
+        />
+        <ProductCard
+          imagem="https://m.media-amazon.com/images/I/41Q2NXnJALL.__AC_SX300_SY300_QL70_ML2_.jpg"
+          nome="Smartfone Multilaser"
+          categoria="Celulares"
+          descricao="Smartphone Multilaser E Pro 4G "
+          preco="R$ 1000,00"
+        />
+        <ProductCard
+          imagem="https://m.media-amazon.com/images/I/41Q2NXnJALL.__AC_SX300_SY300_QL70_ML2_.jpg"
+          nome="Smartfone Multilaser"
+          categoria="Celulares"
+          descricao="Smartphone Multilaser E Pro 4G "
+          preco="R$ 1000,00"
+        />
+        <Col></Col>
       </Row>
     </Container>
   );
-};
+}
 
 export default TesteHome;
-
-
 
 // Nome do componente apenas um teste para criação de rota, caso modificar o nome do componente, modificar o nome do import e a propriedade element no arquivo App.tsx
 // export function TesteHome() {
