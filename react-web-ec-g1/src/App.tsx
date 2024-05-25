c31edf118b71e6f87e83341c62c335a9c109bb62
Lola-snt <lorrayne.work@gmail.com>
2024-05-25 15:30:59 -0300

    Icon carrinho com notificação da quantidade de itens que possui

--- react-web-ec-g1/src/App.tsx
+++ react-web-ec-g1/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -11,31 +11,42 @@ import { TesteProduto } from "./pages/Produto";
 import Button01 from "./components/Button01";
 import "./components/components.css";
 import Form01 from "./components/Form01";
-import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
+import { Badge, Button, Container, Image, Nav, NavDropdown, Navbar } from "react-bootstrap";
 import logo from './assets/img/logo-s.svg'
+import cart from './assets/img/cart4.svg'
+import { CartCtx } from "./Context/CartContext";
+import { FaCartShopping } from "react-icons/fa6";
 
 function App() {
+  const cartctx = useContext(CartCtx)
   return (
     <Container fluid >
       {/* o menu de navegação está fixo, procurando um jeito melhor de fazer */}
       <Navbar fixed="top" expand="lg" className="bg-body-tertiary"  >
-      <Container>
-        <Navbar.Brand href="/">
-          <img src={logo}  width="120"  height="120" ></img>
+        <Container>
+          <Navbar.Brand href="/">
+            <img src={logo} width="120" height="120" ></img>
           </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            {/* <Nav.Link href="/">Home</Nav.Link> */}
-            <Nav.Link href="/login">Login </Nav.Link>
-            <Nav.Link href="/profile">Meu Perfil </Nav.Link>
-            {/* <Nav.Link href="/produto">Produto </Nav.Link> */}
-            <Nav.Link href="/cart">Carrinho </Nav.Link>
-            
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              {/* <Nav.Link href="/">Home</Nav.Link> */}
+              <Nav.Link href="/login">Login </Nav.Link>
+              <Nav.Link href="/profile">Meu Perfil </Nav.Link>
+              {/* <Nav.Link href="/produto">Produto </Nav.Link> */}
+              <Nav.Link href="/cart">
+                <Button variant="light">
+                  <FaCartShopping size={25}/>
+                  <Badge bg="danger">
+                    {cartctx?.totalItems()}
+                  </Badge>
+                </Button>
+              </Nav.Link>
+
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
       <Routes>
         <Route path="/" element={<TesteHome />} />
         <Route path="/login" element={<TesteLogin />} />
