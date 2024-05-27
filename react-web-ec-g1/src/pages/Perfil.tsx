e633b7f119fadd3ec3834463f04c9694b22092e9
Pedro Henrique de Azevedo Wong Rego <phenrique.wong@gmail.com>
2024-05-27 16:42:18 -0300

    Perfil estático

--- react-web-ec-g1/src/pages/Perfil.tsx
+++ react-web-ec-g1/src/pages/Perfil.tsx
@@ -1,45 +1,32 @@
 
-import React from "react";
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
-import './Home.css'
-
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import "./Home.css";
 
 export function TestePerfil() {
   return (
     <Form className="profile">
       <h1>Perfil do usuário</h1>
       <div className="div-profile"></div>
-      <Row className="mb-3">
-        <Form.Group as={Col} controlId="formGridEmail">
-          <Form.Label className="label-profile">Email:</Form.Label>
-          <Form.Control type="email" placeholder="Ex: example@gmail.com" />
-        </Form.Group>
-      </Row>
+
+      <Form.Group as={Col} controlId="formGridEmail">
+        <Form.Label className="label-profile">Email:</Form.Label>
+        <Form.Control type="email" value="jean@hotmail.com" />
+      </Form.Group>
 
       <Form.Group className="mb-3" controlId="formGridAddress1">
         <Form.Label className="label-profile">Nome:</Form.Label>
-        <Form.Control placeholder="Ex: Jean " />
+        <Form.Control value="Jean  " />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formGridAddress2">
         <Form.Label className="label-profile">Cidade</Form.Label>
-        <Form.Control placeholder="Ex: Petrópolis " />
+        <Form.Control value="Petrópolis" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formGridAddress2">
         <Form.Label className="label-profile">Estado</Form.Label>
-        <Form.Control placeholder="Ex: Rio de Janeiro" />
+        <Form.Control value=" Rio de Janeiro" />
       </Form.Group>
-
-      
-
-     
-
-      
     </Form>
-    
-   
   );
 }
